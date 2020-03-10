0b4f2bd79036658e376ac39ead110733d4982f62
ibakyrdjiev <iliya.bakyrdjiev@mentormate.com>
2020-03-10 12:48:16 +0200

    DWH init

---
+++ DWH/JuniorAchievementDataWarehouse/JuniorAchevementDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace JuniorAchievementDataWarehouse
+{
+    using JuniorAchevement.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class JuniorAchevementDbContext : DbContext
+    {
+        public DbSet<Answer> Answers { get; set; }
+
+        public DbSet<Question> Questions { get; set; }
+
+        public DbSet<Template> Templates { get; set; }
+
+        public DbSet<QuestionAnswers> QuestionAnswers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=tcp:wbc-demo.database.windows.net,1433;Initial Catalog=junior-achievement;Persist Security Info=False;User ID=ibakyrdjiev;Password=59nNcKAKMB;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
